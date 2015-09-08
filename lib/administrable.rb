41f96beb5b10302ba5b452fe113c46c1a18c2319
Kurokawa <black.princess.w@gmail.com>
2015-09-09 00:52:26 +0900

    require dependent gems explicitly

--- lib/administrable.rb
+++ lib/administrable.rb
@@ -1,2 +1,13 @@
 module Administrable
 end
+
+
+require 'sass-rails'
+require 'jquery-rails'
+require 'bootstrap-sass'
+require 'coffee-rails'
+require 'haml-rails'
+require 'ransack'
+require 'kaminari'
+
+require 'administrable/engine'
