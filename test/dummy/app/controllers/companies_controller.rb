48febda92c47c47a89e8cc4d4aaaced55c21e783
Kurokawa <black.princess.w@gmail.com>
2015-09-20 16:09:53 +0900

    update example

--- test/dummy/app/controllers/companies_controller.rb
+++ test/dummy/app/controllers/companies_controller.rb
@@ -1,3 +1,15 @@
 class CompaniesController < ApplicationController
   include Administrable::CRUDFeature
+
+  def search_fields
+    [:name_cont]
+  end
+
+  def search_results_fields
+    [:name, :created_at, :updated_at]
+  end
+  
+  def show_fields
+    [:name]
+  end
 end
